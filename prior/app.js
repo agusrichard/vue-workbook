c0e441eceeefbfbd0e13ff364118289cd633ca20
agusrichard <agus.richard21@gmail.com>
2020-05-01 17:18:09 +0700

    Learn #3 -- Data and Methods

--- prior/app.js
+++ prior/app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Agus Richard Lubis'
+    name: 'Agus Richard Lubis',
+    job: 'Ninja'
+  },
+  methods: {
+    greet: function(time) {
+      return `Good ${time}! ${this.name}`
+    }
   }
 });
