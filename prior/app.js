9e753eb11888dd89c47c92f7b64f4e4d12d208c0
agusrichard <agus.richard21@gmail.com>
2020-05-01 17:55:54 +0700

    Lesson #7 -- Keyboard Events

--- prior/app.js
+++ prior/app.js
@@ -1,23 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
-    age: 22,
-    x: 0,
-    y: 0
+    
   },
   methods: {
-    add: function(inc) {
-      this.age += inc
+    logName: function() {
+      console.log('you entered your name')
     },
-    subtract: function(dec) {
-      this.age -= dec
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX
-      this.y = event.offsetY
-    },
-    click: () => {
-      alert('You clicked me!')
+    logAge: function() {
+      console.log('you entered your age')
     }
   }
 });
