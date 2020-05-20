7587d683aa7a2d24d68f0f670152108c2c5c0581
agusrichard <agus.richard21@gmail.com>
2020-05-20 19:49:25 +0700

    Lesson #22 -- Props

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header />
-    <app-ninja />
+    <app-ninja v-bind:ninjas="ninjas" />
     <app-footer />
   </div>
 </template>
@@ -19,7 +19,14 @@ export default {
   },
   data: function() {
     return {
-      
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
     }
   },
  
--- vue-tutorial/src/components/Ninjas.vue
+++ vue-tutorial/src/components/Ninjas.vue
@@ -15,16 +15,15 @@
 <script>
 
 export default {
+  props: {
+    ninjas: {
+      type: Array,
+      required: true
+    }
+  },
   data: function() {
     return {
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ]
+      
     }
   },
 }
