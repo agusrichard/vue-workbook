28d88f6f7410b73908cc59745e4ecaedeaae16f1
agusrichard <agus.richard21@gmail.com>
2020-05-22 09:05:16 +0700

    Lesson #34 -- Custom Directives

--- vue-tutorial/src/main.js
+++ vue-tutorial/src/main.js
@@ -2,12 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 
+Vue.config.productionTip = false
+
 const bus = new Vue()
 export default bus
 
 Vue.use(VueResource)
 
-Vue.config.productionTip = false
+// Custom directive
+Vue.directive('rainbow', {
+  bind(el) {
+    el.style.color = '#' + Math.random().toString().slice(2, 8)
+  }
+})
+
+Vue.directive('theme', {
+  bind(el, binding) {
+    if (binding.value === 'wide') {
+      el.style.maxWidth = '1200px'
+    } else if (binding.value === 'narrow') {
+      el.style.maxWidth = '560px'
+    }
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd'
+      el.style.padding = '20px'
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
