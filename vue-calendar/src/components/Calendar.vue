ff99cdefe3271780c61974f8188734260845b0d1
agusrichard <agus.richard21@gmail.com>
2020-06-07 17:48:31 +0700

    Make some editing

---
+++ vue-calendar/src/components/Calendar.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <v-sheet
+      tile
+      height="54"
+      color="grey lighten-3"
+      class="d-flex"
+    >
+      <v-btn
+        icon
+        class="ma-2"
+        @click="$refs.calendar.prev()"
+      >
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-select
+        v-model="type"
+        :items="types"
+        dense
+        outlined
+        hide-details
+        class="ma-2"
+        label="type"
+      ></v-select>
+      <v-select
+        v-model="mode"
+        :items="modes"
+        dense
+        outlined
+        hide-details
+        label="event-overlap-mode"
+        class="ma-2"
+      ></v-select>
+      <v-select
+        v-model="weekday"
+        :items="weekdays"
+        dense
+        outlined
+        hide-details
+        label="weekdays"
+        class="ma-2"
+      ></v-select>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        class="ma-2"
+        @click="$refs.calendar.next()"
+      >
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-sheet>
+    <v-sheet height="600">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :weekdays="weekday"
+        :type="type"
+        :events="events"
+        :event-overlap-mode="mode"
+        :event-overlap-threshold="30"
+        :event-color="getEventColor"
+        @change="getEvents"
+      ></v-calendar>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    today: new Date().toISOString().substring(0, 10),
+    focus: new Date().toISOString().substring(0, 10),
+    type: 'month',
+    typeToLabel: {
+      month: 'Month',
+      week: 'Week',
+      day: 'Day',
+      '4day': '4 Days'
+    },
+    name: null,
+    details: null,
+    start: null,
+    end: null,
+    color: '#197602'
+  })
+}
+</script>
--- vue-calendar/src/main.js
+++ vue-calendar/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextAreaAutoSize from 'vue-textarea-autosize'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueTextAreaAutoSize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDKZaW35glqvenKgTw5rA1GoMDF4ouJ4tA",
+  authDomain: "vuetify-calendar-32934.firebaseapp.com",
+  databaseURL: "https://vuetify-calendar-32934.firebaseio.com",
+  projectId: "vuetify-calendar-32934",
+  storageBucket: "vuetify-calendar-32934.appspot.com",
+  messagingSenderId: "566135897789",
+  appId: "1:566135897789:web:31fc24c31aedf8305f41d4"
+})
+
+export const db = firebase.firestore()
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
