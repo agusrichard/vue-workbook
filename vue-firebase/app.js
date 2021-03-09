5c79c96f785f39b3c9dd32b9a8197aa4455a8ada
agusrichard <agus.richard21@gmail.com>
2021-03-09 14:24:30 +0700

    More click events, data binding, computed, and challege

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -1,22 +1,51 @@
 const app = Vue.createApp({
   data: () => ({
+    url: 'https://wwww.sekardayu.com',
     name: 'Sekardayu Hana Pradiani',
     occupation: 'Another man\'s wife',
     age: 23,
-    showAge: true
+    showAge: true,
+    x: 0,
+    y: 0,
+    friends: [
+      {
+        name: 'Sekardayu Hana Pradiani',
+        age: 23,
+        isFav: true,
+      },
+      {
+        name: 'Saskia Nurul Azhima',
+        age: 21,
+        isFav: true,
+      },
+      {
+        name: 'Arifa Rachma',
+        age: 23,
+        isFav: true,
+      }
+    ]
   }),
   methods: {
-    increaseAge() {
-      this.age += 1
-    },
-    decreaseAge() {
-      this.age -= 1
-    },
-    changeName(name) {
-      this.name = name
-    },
     toggleShowAge() {
       this.showAge = !this.showAge
+    },
+    handleEvent(event, data) {
+      console.log('event', event)
+      if (data) {
+        console.log('data', data)
+      }
+    },
+    handleMouseMove(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
+    },
+    toggleFav(friend) {
+      friend.isFav = !friend.isFav
+    }
+  },
+  computed: {
+    filteredFriends() {
+      return this.friends.filter(friend => friend.isFav)
     }
   }
 })
