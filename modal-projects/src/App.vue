aeb3d2c44bfe1f380cf046e459f17a11caf1f578
agusrichard <agus.richard21@gmail.com>
2021-03-09 20:17:34 +0700

    A lot of things which I dont quite remember

--- modal-projects/src/App.vue
+++ modal-projects/src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <h1>Sekardayu Hana Pradiani</h1>
+  <h1>{{ title }}</h1>
+  <teleport to=".modals" v-if="showModal">
+    <Modal @close="toggleModal">
+      <h1>Do you love me?</h1>
+      <p>Well, I don't think you do</p>
+      <template v-slot:buttons>
+        <button>Login</button>
+        <button>Sign Up</button>
+      </template>
+    </Modal>
+  </teleport>
+  <div>
+    <button @click="toggleModal">Toggle Modal</button>
+  </div>
 </template>
 
 <script>
+import Modal from "./components/Modal.vue"
+
 export default {
   name: 'App',
-  data: () => ({
-    title: 'Sekardayu Hana Pradiani'
-  })
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'Sekardayu Hana Pradiani',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
 <style>
+html, body {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,9 +51,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1 {
-  background: #ddd;
 }
 </style>
---
+++ modal-projects/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <slot>Default Content</slot>
+      <div>
+        <slot name="buttons"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    width: 500px;
+    height: 250px;
+    background: #fff;
+    border-radius: 10px;
+  }
+
+  .modal h1 {
+    font-size: 40px !important;
+    color: mediumseagreen;
+  }
+
+  .modal p {
+    margin: 20px 0;
+    height: auto;
+  }
+</style>
