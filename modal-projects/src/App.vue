9a822dee82f29eba26be6451444b4681d0e682c1
agusrichard <agus.richard21@gmail.com>
2021-03-09 15:07:09 +0700

    Initialize bigger projects

---
+++ modal-projects/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Sekardayu Hana Pradiani</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    title: 'Sekardayu Hana Pradiani'
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  background: #ddd;
+}
+</style>
