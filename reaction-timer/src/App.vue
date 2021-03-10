d5fc0e0b0be904e1a39a7a4375796e8966ea8cc5
agusrichard <agus.richard21@gmail.com>
2021-03-10 09:53:40 +0700

    Working on reaction timer

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Let's play and know your reaction time</h1>
+  <div class="btn-container">
+    <button class="btn-play" :class="{ disable: isPlaying }" @click="startPlay">Play</button>
+  </div>
+  <Block :delay="delay" v-if="isPlaying" @end="endgame" />
+  <p class="text-reaction">Your reaction time is {{ reactionTime }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      reactionTime: null
+    }
+  },
+  methods: {
+    startPlay() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    },
+    endgame(reactionTime) {
+      console.log('reaction time', reactionTime)
+      this.reactionTime = reactionTime
+      this.isPlaying = false
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+
+.btn-container {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-play {
+  font-size: 22px;
+  display: block;
+  margin: auto;
+  width: 100px;
+  height: 50px;
+  border-style: none;
+  border-color: #ddd;
+  background: coral;
+  color: white;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: 0.25s;
+}
+
+.btn-play:hover {
+  background: blueviolet;
+}
+
+.disable, .btn-play.disable:hover {
+  background: #eee;
+  cursor: not-allowed;
+  color: #999;
+}
+
+.text-reaction {
+  text-align: center;
+}
+</style>>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="block" v-show="showBlock" @click="stopTimer">
+    <p>Click me!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+}
+</script>
+
+<style>
+.block {
+  background-color: coral;
+  width: 500px;
+  height: 300px;
+  margin: 50px auto;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+div.block p {
+  color: white;
+  font-size: 28px;
+}
+</style>
