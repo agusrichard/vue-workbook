576e6249314e032a24d795dd15762f18a6f9802b
agusrichard <agus.richard21@gmail.com>
2021-03-14 02:10:20 +0700

    Learning vuex next level bruh

--- learn-vuex/src/store/index.js
+++ learn-vuex/src/store/index.js
@@ -1,11 +1,25 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: []
   },
   mutations: {
+    setTodos: (state, todos) => state.todos = todos
   },
   actions: {
+    async fetchTodos({ commit }) {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        console.log('fetchTodos', response.data)
+
+        commit('setTodos', response.data)
+      } catch(error) {
+        console.log(error)
+        return error
+      }
+    }
   },
   modules: {
   }
--- learn-vuex/src/views/Todos.vue
+++ learn-vuex/src/views/Todos.vue
@@ -1,14 +1,29 @@
 <template>
   <Container title="Todos">
-    This is a todos page
+    <div>
+      <div v-for="todo in todos" :key="todo.id">{{ todo.title }}</div>
+    </div>
   </Container>
 </template>
 
 <script>
+import { useStore } from 'vuex'
+import { computed, onMounted } from '@vue/runtime-core'
 import Container from '../components/Container.vue'
 
 export default {
-  components: { Container }
+  components: { Container },
+  setup() {
+    const store = useStore()
+
+    console.log('todos', store.state.todos)
+
+    onMounted(() => {
+      store.dispatch('fetchTodos')
+    })
+
+    return { todos: computed(() => store.state.todos) }
+  }
 }
 </script>
 
